bb01bf110ed7325629e59ac4e1e1c15ceb5369da
unknown <jamesverhoef2010@gmail.com>
2026-04-07 21:21:01 -0400

    added a cli tool, ready for first release

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,46 @@
-use std::io;
+use std::{env, fs};
 
-use crate::{interpreter::Interpreter, parser::Parser, lexer::Lexer};
+use crate::{interpreter::Interpreter, lexer::Lexer, parser::Parser};
 
 mod lexer;
 mod parser;
 mod interpreter;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    let mut lexer = Lexer::new(get_input().as_str());
-    let tokens = lexer.tokenize();
-    println!("{:?}", tokens);
+    let args: Vec<String> = env::args().collect();
 
-    let mut parser = Parser::new(tokens);
-    let expressions = parser.parse();
-    println!("{:?}", expressions);
+    match args.get(1) {
+        Some(s) => match s.as_str() {
+            "run" => {
+                let file_name;
+                match args.get(2) {
+                    Some(n) => file_name = n.clone(),
+                    None => file_name = String::from("main.rock"),
+                }
 
-    let mut interpreter = Interpreter::new(expressions);
-    interpreter.run();
-    println!("{:?}", interpreter.variables);
-    println!("{:?}", interpreter.functions);
-}
+                let source = fs::read_to_string(file_name);
+                match source {
+                    Ok(c) => {
+                        let mut lexer = Lexer::new(c.as_str());
+                        let tokens = lexer.tokenize();
 
-fn get_input() -> String {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("failed to read input");
+                        let mut parser = Parser::new(tokens);
+                        let expressions = parser.parse();
 
-    input.trim().to_string()
+                        let mut interpreter = Interpreter::new(expressions);
+                        interpreter.run();
+                    }
+                    Err(_) => eprintln!("invalid file name"),
+                }
+            },
+            _ => eprintln!("unknown command"),
+        },
+        None => {
+            println!("Rockscript version {}", VERSION);
+            println!("Commands:");
+            println!("rockscript run <file>");
+        }
+    }
 }
