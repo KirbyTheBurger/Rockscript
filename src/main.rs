dacb1df58d5bb2d780f24821eb3520b436c7c34d
KirbyTheBurger <jamesverhoef2010@gmail.com>
2026-07-13 14:38:52 -0500

    added a proper cli using clap

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,56 @@
-use std::{env, fs};
+use std::fs;
 
+use clap::Parser as CliParser;
 use logos::Logos;
-
 use crate::{interpreter::Interpreter, lexer::Token, parser::Parser};
+use Commands::*;
 
 mod lexer;
 mod parser;
 mod interpreter;
 
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+#[derive(CliParser)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(clap::Subcommand)]
+enum Commands {
+    /// Run a program (.rock file)
+    Run {
+        file: String,
+        #[arg(short, long)]
+        debug: bool,
+    },
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    match args.get(1) {
-        Some(s) => match s.as_str() {
-            "run" => {
-                let file_name;
-                match args.get(2) {
-                    Some(n) => file_name = n.clone(),
-                    None => file_name = String::from("main.rock"),
-                }
+    match args.command {
+        Run {file, debug} => {
+            let source = fs::read_to_string(file);
+            match source {
+                Ok(c) => {
+                    let lexer = Token::lexer(&c);
+                    let tokens: Vec<Token> = lexer.filter_map(Result::ok).collect();
+                    if debug { println!("Tokens: {:?}", tokens); }
 
-                let source = fs::read_to_string(file_name);
-                match source {
-                    Ok(c) => {
-                        let debug = matches!(args.get(3).map(String::as_str), Some("--debug"));
+                    let mut parser = Parser::new(tokens);
+                    let expressions = parser.parse();
+                    if debug { println!("Expressions: {:?}", expressions); }
 
-                        let lexer = Token::lexer(&c);
-                        let tokens: Vec<Token> = lexer.filter_map(Result::ok).collect();
-                        if debug { println!("Tokens: {:?}", tokens); }
-
-                        let mut parser = Parser::new(tokens);
-                        let expressions = parser.parse();
-                        if debug { println!("Expressions: {:?}", expressions); }
-
-                        let mut interpreter = Interpreter::new(expressions);
-                        interpreter.run();
-                        if debug {
-                            println!("Variables: {:?}", interpreter.variables);
-                            println!("Functions: {:?}", interpreter.functions);
-                        }
+                    let mut interpreter = Interpreter::new(expressions);
+                    interpreter.run();
+                    if debug {
+                        println!("Variables: {:?}", interpreter.variables);
+                        println!("Functions: {:?}", interpreter.functions);
                     }
-                    Err(_) => eprintln!("invalid file name"),
-                }
-            },
-            _ => eprintln!("unknown command"),
-        },
-        None => {
-            println!("Rockscript version {}", VERSION);
-            println!("Commands:");
-            println!("rockscript run <file>");
+                },
+
+                Err(e) => println!("{e}"),
+            }
         }
     }
 }
